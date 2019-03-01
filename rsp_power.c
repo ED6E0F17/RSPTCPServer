896d1e79d7050610073674bb7ac83da120fdf267
hexameron <hexameron>
2019-03-01 14:32:56 +0000

    More rtl_power code.

--- rsp_power.c
+++ rsp_power.c
@@ -2,7 +2,8 @@
 * rtl-sdr, turns your Realtek RTL2832 based DVB dongle into a SDR receiver
 * Copyright (C) 2012 by Steve Markgraf <steve@steve-m.de>
 * Copyright (C) 2012-2013 by Hoernchen <la@tfc-server.de>
-*
+* Copyright (C) 2012 by Kyle Keen <keenerd@gmail.com>
+* 
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 2 of the License, or
@@ -33,13 +34,15 @@
 #include <mirsdrapi-rsp.h>
 
 // temporary fixed resolution of 2.048 Mhz / 1 kHz = 2048 IQ samples
+#define FFT_INDEX (11)
 #define FFT_SIZE (2048)
 
 FILE *file;
 
-int16_t* Sinewave;
-double* power_table;
+int16_t* Sinewave = NULL;
+double* power_table = NULL;
 int16_t *fft_buf;
+int N_WAVE, LOG2_N_WAVE;
 unsigned long *avg = NULL;
 int freq;
 int rate;
@@ -437,6 +440,14 @@ void rx_callback(short* xi, short* xq, unsigned int firstSampleNum, int grChange
 			xi++; xq++;
 		}
 	}
+}
+void get_data()
+{
+	uint32_t line = (buff_now + 3) & 3; // most recently filled
+	fft_buf = &circ_buffer[5 * FFT_SIZE * 2];
+	int16_t *in = &circ_buffer[line * FFT_SIZE * 2];
+	memcpy(fft_buf, in, FFT_SIZE * 2 * sizeof(int16_t));
+	// could copy extra data to wrap around for window function
 }
 
 static rsp_model_t hardware_ver_to_model(int hw_version)
@@ -1134,6 +1145,102 @@ int init_rsp_device(unsigned int sr, unsigned int freq, int enable_bias_t, unsig
 	return 0;
 }
 
+/* FFT based on fix_fft.c by Roberts, Slaney and Bouras
+   http://www.jjj.de/fft/fftpage.html
+   16 bit ints for everything
+   -32768..+32768 maps to -1.0..+1.0
+*/
+
+int sine_table()
+{
+	int i;
+	double d;
+	LOG2_N_WAVE = FFT_INDEX;
+	N_WAVE = 1 << LOG2_N_WAVE;
+	Sinewave = malloc(sizeof(int16_t) * N_WAVE*3/4);
+	power_table = malloc(sizeof(double) * N_WAVE);
+	if (!Sinewave)
+		return 0;
+	if (!power_table)
+		return 0;
+
+	for (i=0; i<N_WAVE*3/4; i++)
+	{
+		d = (double)i * 2.0 * M_PI / N_WAVE;
+		Sinewave[i] = (int)round(32767*sin(d));
+		//printf("%i\n", Sinewave[i]);
+	}
+	return 1;
+}
+
+inline int16_t FIX_MPY(int16_t a, int16_t b)
+/* fixed point multiply and scale */
+{
+	int c = ((int)a * (int)b) >> 14;
+	b = c & 0x01;
+	return (c >> 1) + b;
+}
+
+
+int fix_fft(int16_t iq[])
+/* interleaved iq[], 0 <= n < 2**m, changes in place */
+{
+	int mr, nn, i, j, l, k, istep, n, shift;
+	int16_t qr, qi, tr, ti, wr, wi;
+	int m = FFT_INDEX;
+	n = 1 << m;
+	if (n > N_WAVE)
+		{return -1;}
+	mr = 0;
+	nn = n - 1;
+	/* decimation in time - re-order data */
+	for (m=1; m<=nn; ++m) {
+		l = n;
+		do
+			{l >>= 1;}
+		while (mr+l > nn);
+		mr = (mr & (l-1)) + l;
+		if (mr <= m)
+			{continue;}
+		// real = 2*m, imag = 2*m+1
+		tr = iq[2*m];
+		iq[2*m] = iq[2*mr];
+		iq[2*mr] = tr;
+		ti = iq[2*m+1];
+		iq[2*m+1] = iq[2*mr+1];
+		iq[2*mr+1] = ti;
+	}
+	l = 1;
+	k = LOG2_N_WAVE-1;
+	while (l < n) {
+		shift = 1;
+		istep = l << 1;
+		for (m=0; m<l; ++m) {
+			j = m << k;
+			wr =  Sinewave[j+N_WAVE/4];
+			wi = -Sinewave[j];
+			if (shift) {
+				wr >>= 1; wi >>= 1;}
+			for (i=m; i<n; i+=istep) {
+				j = i + l;
+				tr = FIX_MPY(wr,iq[2*j]) - FIX_MPY(wi,iq[2*j+1]);
+				ti = FIX_MPY(wr,iq[2*j+1]) + FIX_MPY(wi,iq[2*j]);
+				qr = iq[2*i];
+				qi = iq[2*i+1];
+				if (shift) {
+					qr >>= 1; qi >>= 1;}
+				iq[2*j] = qr - tr;
+				iq[2*j+1] = qi - ti;
+				iq[2*i] = qr + tr;
+				iq[2*i+1] = qi + ti;
+			}
+		}
+		--k;
+		l = istep;
+	}
+	return 0;
+}
+
 void scanner(void)
 {
 	int i, j, j2, f, n_read, bin_len, buf_len;
@@ -1143,6 +1250,7 @@ void scanner(void)
 	// single pass for now
 	{
 		get_data(); // update fft_buff
+		// TODO: window function may improve quality
 
 		remove_dc(fft_buf, buf_len);
 		remove_dc(fft_buf + 1, buf_len);
@@ -1378,19 +1486,26 @@ int main(int argc, char **argv)
 		goto out;
 	}
 
+	usleep(5000); // allow time for initial buffers to fill, and sdr to "settle"
+
 	next_tick = time(NULL) + interval;
 	if (exit_time) {
 		exit_time = time(NULL) + exit_time;}
 
+	if (!sine_table())
+		goto out;
 	avg = (unsigned long*)malloc(FFT_SIZE * sizeof(long));
 	if (!avg)
 		goto out;
 
 	while (!do_exit) {
 		scanner();
-		//time_now = time(NULL);
-		//if (time_now < next_tick)
-		//	continue;
+		time_now = time(NULL);
+		if (time_now < next_tick)
+			continue;
+		// loop within each time interval
+
+		// then dump to file
 		// time, Hz low, Hz high, Hz step, samples, dbm, dbm, ...
 		cal_time = localtime(&time_now);
 		strftime(t_str, 50, "%Y-%m-%d, %H:%M:%S", cal_time);
@@ -1403,8 +1518,8 @@ int main(int argc, char **argv)
 			do_exit = 1;
 		if (exit_time && time(NULL) >= exit_time)
 			do_exit = 1;
-		//while (time(NULL) >= next_tick)
-		//	next_tick += interval;
+		while (time(NULL) >= next_tick)
+			next_tick += interval;
 	}
 
 out:
@@ -1413,10 +1528,14 @@ out:
 	mir_sdr_StreamUninit();
 	mir_sdr_ReleaseDeviceIdx();
 
-	if(avg)
+	if (avg)
 		free(avg);
 	if (circ_buffer)
-		free( circ_buffer );
+		free(circ_buffer);
+	if (Sinewave)
+	       free (Sinewave);
+	if (power_table)
+	       free(power_table);
 
 	return 0;
 }
